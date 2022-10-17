489834c47d136f93ae96f02c36737e4980e5a0a9
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-10-16 21:09:20 -0300

    Configuração do banco e geração da primeira migration

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Projeto_MVC.Models;
+
+namespace Projeto_MVC.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Projeto_MVC.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AgendaContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
